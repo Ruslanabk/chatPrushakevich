88eb3643478aed256867ccbc687e8ff2497d9bf2
Ruslan <rusabk@yandex.ru>
2021-02-16 20:26:45 +0300

    lesson finished

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,57 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input type="text" placeholder="Автор" v-model="author">
+    <textarea placeholder="Сообщение" v-model="text"></textarea>
+    <button v-on:click="sendMessage()">Отправить</button>
+    <hr>
+    <br><br>
+
+    <div v-for="(item, i) in messages" :key="i">
+      <strong>{{item.name}}</strong>
+      <p>{{item.text}}</p>
+      <hr>
+    </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      messages: [],
+      author: '',
+      text: ''
+    }
+  },
+  methods: {
+    getMessages(){
+      this.axios({
+        method: "GET",
+        url: "http://37.77.104.246/api/jsonstorage/?id=db97868f606182092b61089701130650"
+      }).then( response => {
+        this.messages = response.data;
+      } )
+    },
+    sendMessage(){
+      this.getMessages();
+      let newMessage = {
+        name: this.author,
+        text: this.text
+      }
+      this.messages.push(newMessage);
+      this.axios({
+        method: "PUT",
+        url: "http://37.77.104.246/api/jsonstorage/?id=db97868f606182092b61089701130650",
+        data: this.messages
+      });
+    }
+  },
+  mounted(){
+    setInterval(this.getMessages, 1000);
+    this.getMessages();
   }
 }
 </script>
@@ -21,7 +61,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
---
+++ src/components/Button.vue
@@ -0,0 +1,24 @@
+<template>
+    <button class="btn" v-bind:style=" 'background: ' + color ">
+        <slot>Текст по умолчанию</slot>
+    </button>
+</template>
+
+<script>
+export default {
+    name: 'Button',
+    props: ['color', 'text']
+}
+</script>
+
+<style>
+.btn{
+    padding: 10px 20px;
+    color: #fff;
+    background-color: #6c5ce7;
+    border-radius: 10px;
+    border: 0;
+    outline: none;
+    cursor: pointer;
+}
+</style>
